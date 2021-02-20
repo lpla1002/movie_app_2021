b1e90e872a7e2c91031da6494195e9d58d363c73
lpla1002 <lpla1002@naver.com>
2021-02-20 20:10:14 +0900

    add md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ReactJS로 영화 웹 서비스 만들기 (2021)
+
+2021년 2월 6일 ~ 20일까지 [**노마드코더**](https://nomadcoders.co/react-fundamentals) 강의를 수강하고 공부한 내용을 정리했습니다.
+자세한 내용은 [**블로그**](https://lpla.tistory.com/)를 확인해주세요.
---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function App() {
+  return (
+    <div className="about_con">
+      <p>About페이지입니다.</p>
+      <p>영화 웹서비스 만들기 공부 중입니다.</p>
+    </div>
+  )
+}
+
+export default App;
